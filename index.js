af1928b6143eb7150d944771d71ecdd335dc26e7
ScreamingGrandma <Keanu.Ngov@Gmail.com>
2025-05-10 12:43:16 -0700

    set: firebase

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/11.7.1/firebase-database.js";
+const firebaseConfig = {
+  databaseURL: "https://mobile-app-7c351-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const shoppingListDB = ref(database, "ScreamingGrandma");
+
 const inputFieldEl = document.getElementById("input-field");
 const buttonAddEl = document.getElementById("button-add");
 
 buttonAddEl.addEventListener("click", function () {
   if (inputFieldEl.value) {
     const inputFieldValue = inputFieldEl.value;
+    push(shoppingListDB, inputFieldValue);
     console.log(inputFieldValue);
+
+    inputFieldEl.value = "";
   }
 });
